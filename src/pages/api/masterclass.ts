c9497754deb823b3ee3e659ab267bc2d1873c6f1
alisuperpro <alisolorzanove@gmail.com>
2026-02-22 15:35:47 -0400

    feat: add banner component and form for master class registration
    - Added a new Banner component to display the master class information.
    - Created CheckBox and Input components for form fields.
    - Implemented a registration form in the index page with various input fields.
    - Added API route for handling form submissions and storing participant data.
    - Integrated Tailwind CSS for styling and layout.
    - Removed the Welcome component as it was no longer needed.
    - Added global CSS file for Tailwind imports.
    - Set up Turso client for database interactions.

---
+++ src/pages/api/masterclass.ts
@@ -0,0 +1,106 @@
+import type { APIRoute } from "astro"
+import { turso } from "../../turso"
+import { randomUUID } from "node:crypto"
+
+import { put } from "@vercel/blob"
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData()
+  const name = data.get("fullname")
+  const dni = data.get("dni")
+  const email = data.get("email")
+  const phone = data.get("phone")
+  const igUsername = data.get("ig-input")
+  const findUs = data.get("find-us")
+  const underAge = data.get("under-age")
+  const disability = data.get("disability")
+  const work = data.get("work")
+  const university = data.get("university")
+  const experience = data.get("experience")
+  const ref = data.get("ref")
+  const otherWork = data.get("other-work-input-text")
+  const otherUniversity = data.get("other-university-input-text")
+  const payImg = data.get("pay")
+
+  const id = randomUUID()
+  const splitted = payImg?.name?.split(".")[1]
+
+  const rename = `${id}.${splitted}`
+
+  /* console.log({
+    name,
+    dni,
+    email,
+    phone,
+    igUsername,
+    findUs,
+    underAge,
+    disability,
+    work,
+    university,
+    experience,
+    ref,
+    otherWork,
+    otherUniversity,
+    payImg,
+  }) */
+
+  try {
+    const rs = await turso.execute({
+      sql: "INSERT INTO participants (id, name, dni, email,phone, ig_username, find_us, under_age, disability, work, university, experience, pay_ref, pay_img) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+      args: [
+        id,
+        name?.toString(),
+        dni?.toString(),
+        email?.toString(),
+        phone?.toString(),
+        igUsername?.toString(),
+        findUs?.toString(),
+        underAge?.toString(),
+        disability?.toString(),
+        work?.toString() === "Otro" ? otherWork?.toString() : work?.toString(),
+        university?.toString() === "Otro"
+          ? otherUniversity?.toString()
+          : university?.toString(),
+        experience?.toString(),
+        ref?.toString(),
+        rename,
+      ],
+    })
+
+    const { rows } = await turso.execute({
+      sql: "SELECT * FROM participants WHERE id = ?",
+      args: [id],
+    })
+
+    return new Response(
+      JSON.stringify({
+        data: rows,
+        message: "Success!",
+      }),
+      { status: 200 },
+    )
+  } catch (err) {
+    console.log(err)
+  }
+  //const splitted = payImg?.name?.split('.')[0]
+
+  //const rename = `${}`
+
+  // Validate the data - you'll probably want to do more than this
+  if (!name || !email) {
+    return new Response(
+      JSON.stringify({
+        message: "Missing required fields",
+      }),
+      { status: 400 },
+    )
+  }
+  // Do something with the data, then return a success response
+  return new Response(
+    JSON.stringify({
+      message: "Success!",
+    }),
+    { status: 200 },
+  )
+}
