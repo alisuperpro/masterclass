d62db4b342e2cd75b7a076be2ad67591be98d09a
alisuperpro <alisolorzanove@gmail.com>
2026-03-04 14:48:02 -0400

    Update redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server"
+import { API_URL } from "./lib/const"
 
 const isProtectedRoute = createRouteMatcher([
   "/perfil(.*)",
@@ -6,12 +7,55 @@ const isProtectedRoute = createRouteMatcher([
   "/audiovisual/formulario(.*)",
 ])
 
-export const onRequest = clerkMiddleware((auth, context) => {
-  const { isAuthenticated, redirectToSignIn } = auth()
+const isInfoAdditionalRoute = createRouteMatcher(["/informacion-adicional(.*)"])
+const isProfileRoute = createRouteMatcher(["/perfil(.*)"])
+
+export const onRequest = clerkMiddleware(async (auth, context, next) => {
+  const { isAuthenticated, redirectToSignIn, userId, getToken } = auth()
 
   if (!isAuthenticated && isProtectedRoute(context.request)) {
-    // Add custom logic to run before redirecting
-
     return redirectToSignIn()
   }
+
+  if (
+    userId &&
+    isProtectedRoute(context.request) &&
+    !isInfoAdditionalRoute(context.request)
+  ) {
+    try {
+      const token = await getToken()
+      const result = await verifyUserData({ userId, token })
+
+      if (result.res.status === 404) {
+        return context.redirect("/informacion-adicional")
+      }
+
+      return next()
+    } catch (error) {
+      console.error("Error verificando usuario:", error)
+      return next() // En caso de error, continuar (o podrías mostrar una página de error)
+    }
+  }
+
+  // Para cualquier otro caso, continuar
+  return next()
 })
+
+export async function verifyUserData({
+  userId,
+  token,
+}: {
+  userId: string | null
+  token: string | null
+}) {
+  const res = await fetch(`${API_URL}/api/user/me/${userId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const json = await res.json()
+  return { res, json }
+}
